0d5bcf73cdc745e8b0017e6bb36a2c5e10af5e24
sajilotaxi <sajilotaxi@gmail.com>
2025-10-14 11:57:05 +0530

    Initial commit

---
+++ sw.js
@@ -0,0 +1,71 @@
+const CACHE_NAME = 'sajilo-taxi-cache-v1';
+const urlsToCache = [
+  '/',
+  '/index.html',
+  '/index.tsx',
+  '/icons/icon-192x192.png',
+  '/icons/icon-512x512.png'
+];
+
+self.addEventListener('install', (event) => {
+  event.waitUntil(
+    caches.open(CACHE_NAME)
+      .then((cache) => {
+        console.log('Opened cache');
+        return cache.addAll(urlsToCache);
+      })
+  );
+});
+
+self.addEventListener('fetch', (event) => {
+  event.respondWith(
+    caches.match(event.request)
+      .then((response) => {
+        // Cache hit - return response
+        if (response) {
+          return response;
+        }
+
+        const fetchRequest = event.request.clone();
+
+        return fetch(fetchRequest).then(
+          (response) => {
+            // Check if we received a valid response
+            if (!response || response.status !== 200) {
+              return response;
+            }
+            // We don't cache non-basic responses (e.g., from CDNs) here
+            // to avoid CORS issues, but the browser will cache them normally.
+            if(response.type !== 'basic'){
+                 return response;
+            }
+
+
+            const responseToCache = response.clone();
+
+            caches.open(CACHE_NAME)
+              .then((cache) => {
+                cache.put(event.request, responseToCache);
+              });
+
+            return response;
+          }
+        );
+      })
+  );
+});
+
+self.addEventListener('activate', (event) => {
+  const cacheWhitelist = [CACHE_NAME];
+  event.waitUntil(
+    caches.keys().then((cacheNames) => {
+      return Promise.all(
+        cacheNames.map((cacheName) => {
+          if (cacheWhitelist.indexOf(cacheName) === -1) {
+            return caches.delete(cacheName);
+          }
+        })
+      );
+    })
+  );
+});
